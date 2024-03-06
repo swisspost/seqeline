9ad44f55e1cd2ef5137831efe9e70436cec56126
Laurent Bovet <laurent.bovet@windmaster.ch>
2024-03-06 12:06:12 +0100

    feat: integrate parser

--- src/main/java/ch/post/tools/seqeline/Main.java
+++ src/main/java/ch/post/tools/seqeline/Main.java
@@ -4,40 +4,160 @@ import ch.post.tools.seqeline.catalog.Schema;
 import ch.post.tools.seqeline.process.TreeProcessor;
 import lombok.SneakyThrows;
 import lombok.extern.java.Log;
-import lombok.extern.slf4j.Slf4j;
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.model.util.ModelBuilder;
-import org.eclipse.rdf4j.model.vocabulary.RDF;
-import org.eclipse.rdf4j.model.vocabulary.RDFS;
-import org.eclipse.rdf4j.rio.RDFFormat;
-import org.eclipse.rdf4j.rio.RDFWriter;
-import org.eclipse.rdf4j.rio.Rio;
-import org.joox.Context;
-import org.joox.Match;
-import org.w3c.dom.Element;
+import org.antlr.grammars.plsql.PlSqlLexer;
+import org.antlr.grammars.plsql.PlSqlParser;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import org.apache.commons.text.StringEscapeUtils;
 import org.xml.sax.SAXException;
 
-import javax.xml.catalog.Catalog;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static org.eclipse.rdf4j.model.util.Values.iri;
-import static org.eclipse.rdf4j.model.util.Values.literal;
-import static org.joox.JOOX.$;
+import java.io.*;
+import java.util.Set;
 
 @Log
 public class Main {
     public static void main(String[] args) throws IOException, SAXException {
-        if (args.length < 3) {
-            log.severe("seqeline <domain> <application> <filename>");
-            System.exit(1);
+        if(args.length > 0 && args[0].equals("-p")) {
+
+            InputStream inputStream = new FileInputStream(args[1]);
+
+            var lexer = new PlSqlLexer(CharStreams.fromStream(inputStream));
+            var tokenStream = new CommonTokenStream(lexer);
+            var parser = new PlSqlParser(tokenStream);
+            var parseTree = parser.sql_script();
+            var walker = new ParseTreeWalker();
+
+            var out = args.length > 2 ?  new FileOutputStream(args[2]) : System.out;
+            var writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write("<?xml version=\"1.0\"?>");
+            writer.newLine();
+            var listener = new ParseTreeListener() {
+
+                private String indent =  "";
+                private boolean showTerminal = false;
+                private int ignoreDepth = 0;
+
+                @SneakyThrows
+                @Override
+                public void visitTerminal(TerminalNode terminalNode) {
+                    if(showTerminal) {
+                        writer.write(indent + StringEscapeUtils.escapeXml11(terminalNode.toString()));
+                        writer.newLine();
+                    }
+                }
+
+                @Override
+                public void visitErrorNode(ErrorNode errorNode) {
+                    throw new RuntimeException(errorNode.toString());
+                }
+
+                @SneakyThrows
+                @Override
+                public void enterEveryRule(ParserRuleContext parserRuleContext) {
+                    var name = name(parser, parserRuleContext);
+                    if(ignore(name)) {
+                        ignoreDepth++;
+                    }
+                    if(ignoreDepth == 0) {
+                        if (show(name)) {
+                            writer.write(indent + "<" + name + ">");
+                            writer.newLine();
+                            indent = indent + "  ";
+                        }
+                    }
+                    if(terminal(name)) {
+                        showTerminal = true;
+                    }
+                }
+
+                @SneakyThrows
+                @Override
+                public void exitEveryRule(ParserRuleContext parserRuleContext) {
+                    var name = name(parser, parserRuleContext);
+                    if(ignoreDepth == 0) {
+                        if (show(name)) {
+                            indent = indent.substring(0, indent.length() - 2);
+                            writer.write(indent + "</" + name + ">");
+                            writer.newLine();
+                        }
+                    }
+                    if(ignore(name)) {
+                        ignoreDepth--;
+                    }
+                    if(terminal(name)) {
+                        showTerminal = false;
+                    }
+                }
+            };
+            walker.walk(listener, parseTree);
+            writer.flush();
+
+        } else {
+            if (args.length < 3) {
+                log.severe("seqeline <domain> <application> <filename>");
+                System.exit(1);
+            }
+            Schema schema = new Schema("data/model/metadata.json");
+            new TreeProcessor(args[0], args[1], args[2], schema).process("target/out.trig");
         }
-        Schema schema = new Schema("data/model/metadata.json");
-        new TreeProcessor(args[0], args[1], args[2], schema).process("target/out.trig");
     }
+
+    private static String name(PlSqlParser parser, ParserRuleContext parserRuleContext) {
+        return parserRuleContext.toString(parser, parserRuleContext.getParent())
+                .replaceAll("[\\[\\]]", "")
+                .trim();
+    }
+
+    private static boolean show(String name) {
+        return !skipped.contains(name);
+    }
+
+    private static boolean ignore(String name) {
+        return ignored.contains(name);
+    }
+
+    private static boolean terminal(String name) {
+        return terminal.contains(name);
+    }
+
+    private static Set<String> terminal = Set.of(
+            "id_expression",
+            "constant"
+    );
+
+    private static Set<String> ignored = Set.of(
+            "type_spec",
+            "exception_declaration"
+    );
+
+    private static Set<String> skipped = Set.of(
+            "unit_statement",
+            "package_obj_spec",
+            "package_obj_body",
+            "atom",
+            "standard_function",
+            "seq_of_declare_specs",
+            "declare_spec",
+            "select_statement",
+
+            "regular_id",
+            "non_reserved_keywords_pre12c",
+
+            "model_expression",
+            "logical_expression",
+            "multiset_expression",
+            "unary_logical_expression",
+            "relational_expression",
+            "compound_expression",
+            "unary_expression",
+            "concatenation",
+
+            "quoted_string",
+            "numeric",
+
+            "function_argument",
+
+            "transaction_control_statements",
+            "commit_statement"
+    );
 }
